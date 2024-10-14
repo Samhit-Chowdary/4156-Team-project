1cea63595c5ebbed8c8a793781167b8f278994a1
Madhura Chatterjee <madhurachatterjee@Madhuras-Laptop.local>
2024-10-14 14:17:43 -0400

    Modifying EmployeeProfile.java basd on existing DB

--- NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/model/EmployeeProfile.java
+++ NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/model/EmployeeProfile.java
@@ -14,49 +14,57 @@ import io.micrometer.common.lang.NonNull;
 
 /**
  * Database fields for an employee's profile
- * Contains fields: id, name, phone number, gender, address,
+ * Contains fields: id, name, phone number, gender, base salary,
  *                  age, start date, email and emergency contact
  */
 @Data
 @Entity
-@Table(name = "employeeProfile")
+@Table(name = "Employee", uniqueConstraints = {@UniqueConstraint(columnNames = {"Phone_Number", "Email_Id"})
+        })
 public class EmployeeProfile implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer Id;
 
     @NonNull
+    @Size(min = 1, max = 255)
     private String name;
 
     @NonNull
-    @Size(max = 17, message = "A phone number can have at max of 17 characters")
+    @Size(min = 10, max = 17)
+    @Column(name = "Phone_number")
     private String phoneNumber;
 
     @NonNull
+    @Size(min = 1, max = 255)
     private String gender;
-
-    @NonNull
-    private String address;
 
     @NonNull
     @Min(18)
     private Integer age;
 
     @NonNull
+    @Column(name = "Start_Date")
     private LocalDate startDate;
 
     @NonNull
+    @Size(min = 1, max = 255)
+    private String designation;
+
+    @NonNull
     @Email
+    @Size(min = 5, max = 255)
+    @Column(name = "Email_Id")
     private String email;
 
     @NonNull
-    private String emergencyContactName;
-
-    @NonNull
-    private String emergencyContactRelation;
-
-    @NonNull
-    @Size(max = 17, message = "A phone number can have at max of 17 characters")
+    @Size(min = 10, max = 17)
+    @Column(name = "Emergency_Contact_Number")
     private String emergencyContactNumber;
+
+    @NonNull
+    @Min(0)
+    @Column(name = "Base_Salary")
+    private Integer baseSalary;
     
 }
