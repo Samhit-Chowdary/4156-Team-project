ae0b6758d6398dd481693c276391d108a385a096
AbhilashGanga <saiabhialsh125@gmail.com>
2024-10-15 12:15:44 +0530

    Update getPayrollByEmployeeID and Add markPaid and markUnpaid APIs

--- NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/controller/PayrollController.java
+++ NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/controller/PayrollController.java
@@ -2,30 +2,74 @@ package com.nullterminators.project.controller;
 
 import com.nullterminators.project.model.Payroll;
 import com.nullterminators.project.service.PayrollService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/payroll")
 public class PayrollController {
 
-    @Autowired
-    private PayrollService payrollService;
+    private final PayrollService payrollService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Payroll> getPayroll(@PathVariable("id") Integer id) {
+    @Autowired
+    public PayrollController(PayrollService payrollService) {
+        this.payrollService = payrollService;
+    }
+
+    @GetMapping(value = "/{employeeId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getPayrollByEmployeeId(@PathVariable("employeeId") Integer employeeId) {
         try{
-            Payroll payroll = payrollService.getPayroll(id);
-            if (payroll == null) {
-                return ResponseEntity.notFound().build();
+            String result = payrollService.getPayrollByEmployeeId(employeeId);
+            if (result.isEmpty()) {
+                return new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
             }
-            return ResponseEntity.ok(payroll);
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (Exception e) {
-            return ResponseEntity.status(500).build();
+            return handleException(e);
         }
+    }
+
+    @PatchMapping(value = "/{employeeId}/markPaid", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> markAsPaid(@PathVariable("employeeId") Integer employeeId, @RequestBody Map<String, Object> updates) {
+        try{
+            Integer result = payrollService.markAsPaid(employeeId, (String) updates.get("paymentMonth"), (String) updates.get("paymentYear"));
+            return switch (result) {
+                case 0 -> new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
+                case 1 -> new ResponseEntity<>("Payroll for this month is Not Found", HttpStatus.BAD_REQUEST);
+                case 2 -> new ResponseEntity<>("Employee has already been paid", HttpStatus.BAD_REQUEST);
+                case 3 -> new ResponseEntity<>("Attribute was updated successfully", HttpStatus.OK);
+                default -> new ResponseEntity<>("An Error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+            };
+        } catch (Exception e) {
+            return handleException(e);
+        }
+    }
+
+    @PatchMapping(value = "/{employeeId}/markUnpaid", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> markAsUnpaid(@PathVariable("employeeId") Integer employeeId, @RequestBody Map<String, Object> updates) {
+        try{
+            Integer result = payrollService.markAsUnpaid(employeeId, (String) updates.get("paymentMonth"), (String) updates.get("paymentYear"));
+            return switch (result) {
+                case 0 -> new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
+                case 1 -> new ResponseEntity<>("Payroll for this month is Not Found", HttpStatus.BAD_REQUEST);
+                case 2 -> new ResponseEntity<>("Employee has not been paid", HttpStatus.BAD_REQUEST);
+                case 3 -> new ResponseEntity<>("Attribute was updated successfully", HttpStatus.OK);
+                default -> new ResponseEntity<>("An Error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+            };
+        } catch (Exception e) {
+            return handleException(e);
+        }
+    }
+
+    private ResponseEntity<?> handleException(Exception e) {
+        System.out.println(e.toString());
+        return new ResponseEntity<>("An Error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/service/PayrollService.java
+++ NullTerminatorsTeamProject/src/main/java/com/nullterminators/project/service/PayrollService.java
@@ -5,14 +5,71 @@ import com.nullterminators.project.repository.PayrollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.util.List;
+
 @Service
 public class PayrollService {
 
-    @Autowired
-    private PayrollRepository payrollRepository;
+    private final PayrollRepository payrollRepository;
 
-    public Payroll getPayroll(Integer id) {
-        return payrollRepository.findById(id).orElse(null);
+    @Autowired
+    public PayrollService(PayrollRepository payrollRepository) {
+        this.payrollRepository = payrollRepository;
+    }
+
+    public String getPayrollByEmployeeId(Integer employeeId) {
+        List<Payroll> payrollInformation = payrollRepository.findAllByEmployeeIdOrderByPaymentDateDesc(employeeId);
+        StringBuilder result = new StringBuilder();
+
+        for (Payroll payroll : payrollInformation) {
+            result.append("Payment Date: ").append(payroll.getPaymentDate()).append("\n");
+            result.append("Payslip: ").append(payroll.getPayslip()).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    public Integer markAsPaid(Integer employeeId, String paymentMonth, String paymentYear) {
+        Integer month = Integer.parseInt(paymentMonth);
+        Integer year = Integer.parseInt(paymentYear);
+        Payroll payroll = payrollRepository.findByEmployeeIdOrderByPaymentMonthAndYear(employeeId, month, year);
+        if (payroll == null) { return 0; }
+        else {
+            Month latestPaymentMonth = payroll.getPaymentDate().getMonth();
+            Month currentMonth = LocalDate.now().getMonth();
+            if (!latestPaymentMonth.equals(currentMonth)) { return 1; }
+            else {
+                if (payroll.getPaid() == 1) { return 2; }
+                else {
+                    payroll.setPaid(1);
+                    payrollRepository.save(payroll);
+                    return 3;
+                }
+            }
+        }
+    }
+
+    public Integer markAsUnpaid(Integer employeeId, String paymentMonth, String paymentYear) {
+        Integer month = Integer.parseInt(paymentMonth);
+        Integer year = Integer.parseInt(paymentYear);
+        Payroll payroll = payrollRepository.findByEmployeeIdOrderByPaymentMonthAndYear(employeeId, month, year);
+        if (payroll == null) { return 0; }
+        else {
+            Month latestPaymentMonth = payroll.getPaymentDate().getMonth();
+            Month currentMonth = LocalDate.now().getMonth();
+            if (!latestPaymentMonth.equals(currentMonth)) { return 1; }
+            else {
+                if (payroll.getPaid() == 0) { return 2; }
+                else {
+                    payroll.setPaid(0);
+                    payrollRepository.save(payroll);
+                    return 3;
+                }
+            }
+        }
     }
 
 }
